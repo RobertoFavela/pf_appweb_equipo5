d4af5dd1d361dd2f3856592b73540ea6cbb6c055
Jesus Morales <jesus.morales245335@potros.itson.edu.mx>
2024-10-28 22:45:51 -0700

    avancito
    Co-Authored-By: RobertoFavela <160526441+RobertoFavela@users.noreply.github.com>

---
+++ Episodico_presentacion/src/main/java/LogIn/LogInModel.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LogIn;
+
+/**
+ *
+ * @author tacot
+ */
+public class LogInModel {
+    
+}
